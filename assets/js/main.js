de75b5e7c3f6b2ce56ab7f21a02be37bf1a3f696
Underemployed <underemployed871@gmail.com>
2025-09-24 21:54:45 +0530

    nav fix + pop up

---
+++ assets/js/main.js
@@ -0,0 +1,142 @@
+// desktop nav bar link selection
+var header = document.getElementById("header");
+var nav_items = header.getElementsByClassName("nav-item");
+for (var i = 0; i < nav_items.length; i++) {
+    nav_items[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("prsnt");
+        current[0].className = current[0].className.replace(" prsnt", "");
+        this.className += " prsnt";
+    });
+}
+
+// mobile nav bar slider
+// const navigation = document.querySelector('.navigation');
+// const navContainer = document.querySelector('.nav-container');
+// document.querySelector('.nav-btn').onclick = function(){
+//     this.classList.toggle('active');
+//     naviContainer.classList.toggle('active')
+// }
+// mobile nav bar slider (above code has another logic)
+function openNav() {
+    const navContainer = document.querySelector('.nav-container');
+    const closeBtn = document.querySelector('.closebtn');
+    navContainer.classList.toggle('active')
+    closeBtn.classList.toggle('active')
+}
+function closeNav() {
+    const navContainer = document.querySelector('.nav-container');
+    const closeBtn = document.querySelector('.closebtn');
+    navContainer.classList.toggle('active')
+    closeBtn.classList.toggle('active')
+}
+
+
+
+function showEvent_1_Details() {
+    let blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    let popup = document.getElementById('pop-up1');
+    popup.classList.toggle('active')
+    let html = document.getElementById('html');
+    html.classList.toggle('active')
+}
+
+
+function showEvent_2_Details() {
+    let blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    let popup = document.getElementById('pop-up2');
+    popup.classList.toggle('active')
+    let html = document.getElementById('html');
+    html.classList.toggle('active')
+}
+
+function showEvent_3_Details() {
+    let blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    let popup = document.getElementById('pop-up3');
+    popup.classList.toggle('active')
+    let html = document.getElementById('html');
+    html.classList.toggle('active')
+}
+
+
+function showEvent_4_Details() {
+    let blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    let popup = document.getElementById('pop-up4');
+    popup.classList.toggle('active')
+    let html = document.getElementById('html');
+    html.classList.toggle('active')
+}
+
+
+
+
+function showEvent_5_Details() {
+    let blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    let popup = document.getElementById('pop-up5');
+    popup.classList.toggle('active')
+    let html = document.getElementById('html');
+    html.classList.toggle('active')
+}
+
+
+function showEvent_6_Details() {
+    let blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    let popup = document.getElementById('pop-up6');
+    popup.classList.toggle('active')
+    let html = document.getElementById('html');
+    html.classList.toggle('active')
+}
+
+
+function showEvent_7_Details() {
+    let blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    let popup = document.getElementById('pop-up7');
+    popup.classList.toggle('active')
+    let html = document.getElementById('html');
+    html.classList.toggle('active')
+}
+
+
+
+function showEvent_8_Details() {
+    let blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+    let popup = document.getElementById('pop-up8');
+    popup.classList.toggle('active')
+    let html = document.getElementById('html');
+    html.classList.toggle('active')
+}
+
+
+// Load Past event divs (please dont make further modifications)
+function loadPastEvents(pageNo = 1) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `events/pages/page${pageNo}.html`, true);
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            let container = document.getElementById('past-content');
+
+            if (!container) {
+                container = document.createElement('div');
+                container.id = 'past-content';
+                document.body.appendChild(container);
+            }
+
+            container.innerHTML = xhr.responseText;
+        }
+    };
+
+    xhr.send();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (window.location.pathname.includes("pastEvents.html")) {
+        loadPastEvents();
+    }
+});
